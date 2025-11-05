7f90c5c88e818dd5547986fcba7916b851a0b340
Remko Jantzen <693172+remkoj@users.noreply.github.com>
2025-11-05 23:05:46 +0100

    Added Dockerfile

--- Program.cs
+++ Program.cs
@@ -21,8 +21,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     //app.UseHsts();
 
-    // Enable HTTPS
-    app.UseHttpsRedirection();
+    // Enable HTTPS - Disabled because Render.com does this at infra level
+    //app.UseHttpsRedirection();
 } else
 {
     app.UseDeveloperExceptionPage();
